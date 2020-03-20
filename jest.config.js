38809fee317c65637660c6fd55124e89b8734af0
RITO3 <25099791+RITO3@users.noreply.github.com>
2020-03-21 00:02:44 +0900

    Jenkins連携追加.

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,35 @@
 module.exports = {
-  roots: ['./src'],
+  roots: ["./src"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest"
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
   collectCoverage: true,
-  coverageDirectory: '<rootDir>/result/jest/coverage',
-  coverageReporters: ['html'],
+  coverageDirectory: "<rootDir>/result/jest/coverage",
+  coverageReporters: ["html", "cobertura"],
   collectCoverageFrom: [
-    './src/**/*.ts',
-    '!./src/**/*.d.ts',
-    '!./src/**/*.spec.ts',
-    '!./src/**/*.test.ts',
-    '!./src/**/__*__/*',
+    "./src/**/*.ts",
+    "!./src/**/*.d.ts",
+    "!./src/**/*.spec.ts",
+    "!./src/**/*.test.ts",
+    "!./src/**/__*__/*"
   ],
   reporters: [
-    'default',
+    "default",
     [
-      'jest-html-reporters',
+      "jest-junit",
       {
-        publicPath: './result/jest/html-report',
-        filename: 'report.html',
-        expand: true,
-      },
+        outputDirectory: "./result/jest",
+        outputName: "junit.xml"
+      }
     ],
-  ],
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./result/jest/html-report",
+        filename: "report.html",
+        expand: true
+      }
+    ]
+  ]
 };
